34403a9244485c50de6842c421c18a92cef460b2
AdayaNezer <adaya1027@gmail.com>
2023-05-10 15:09:26 +0300

    fix you

--- app_youtube_download.py
+++ app_youtube_download.py
@@ -6,29 +6,38 @@ VIDEO_SAVE_DIRECTORY = "./videos"
 AUDIO_SAVE_DIRECTORY = "./audio"
 
 
-def download(video_url):
-    video = YouTube(video_url)
-    video = video.streams.get_highest_resolution()
+# def download(video_url):
+#     video = YouTube(video_url)
+#     st.write("Title of Video: " + str(video.title))
+#     st.write("Number of Views: " + str(video.views))
+#     video = video.streams.get_highest_resolution()
 
-    try:
-        st.write(os.path.expanduser("~/Downloads"))
-        video.download(os.path.expanduser("~/Downloads"))
-    except:
-        print("Failed to download video")
+#     try:
+#         # st.download_button(
+#         #     label="Download data",
+#         #     data=video.download(),
+#         #     file_name=f'vvv.mp4',
+#         # )
+#         video.download()
+#     except:
+#         print("Failed to download video")
 
-    print("video was downloaded successfully")
+#     print("video was downloaded successfully")
 
 
-def download_audio(video_url):
-    video = YouTube(video_url)
-    audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
+# def download_audio(video_url):
+#     video = YouTube(video_url)
+#     st.write("Title of Video: " + str(video.title))
+#     st.write("Number of Views: " + str(video.views))
+#     audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
 
-    try:
-        audio.download(os.path.expanduser("~/Downloads"))
-    except:
-        print("Failed to download audio")
+#     try:
+#         video.streams.get_audio_only().download()
+#         audio.download(os.path.expanduser("~/Downloads"))
+#     except:
+#         print("Failed to download audio")
 
-    print("audio was downloaded successfully")
+#     print("audio was downloaded successfully")
 
 
 st.markdown(
@@ -48,18 +57,26 @@ st.markdown('<h1 style="color: #e31072 ">Download videos and songs from YouTube<
 
 st.markdown('<h3 style=" color: #e31072 ">To download please enter a link</h3>',
             unsafe_allow_html=True)
-URL = st.text_input('')
+url = st.text_input('')
+
 st.markdown('<h3 style=" color: #e31072 ">Please select the file type</h3>',
             unsafe_allow_html=True)
 type = st.radio('',
                 ('Video', 'Audio'))
+if url != '':
+    video = YouTube(url)
+    st.markdown(
+        f'<h3 style=" color: #e31072 ">Title of Video:</h3> <h5> { str(video.title)} </h5>', unsafe_allow_html=True)
+    st.markdown(
+        f'<h3 style=" color: #e31072 ">Number of Views:</h3> <h5>  { str(video.views)}</h5>', unsafe_allow_html=True)
+    st.video(url)
+    if st.button('Download now'):
+        if type == 'Video':
+            video.streams.get_highest_resolution().download()
 
-if st.button('Download now'):
-    if type == 'Video':
-        download(URL)
-        st.markdown('<h3 color: " #e31072 "> Downloaded successfully</h3>',
-                    unsafe_allow_html=True)
-    else:
-        download_audio(URL)
-        st.markdown('<h3 style=" color: #e31072 "> Downloaded successfully</h3>',
-                    unsafe_allow_html=True)
+        else:
+            video.streams.get_audio_only().download()
+    print("video was downloaded successfully")
+
+    st.markdown('<h3 style= color: " #e31072 "> Downloaded successfully</h3>',
+                unsafe_allow_html=True)
