8b809c184659bb6a016aad6c9249ed2f8e8c9760
AdayaNezer <adaya1027@gmail.com>
2023-05-10 12:45:37 +0300

    Delete one file and onion to one

--- app_youtube_download.py
+++ app_youtube_download.py
@@ -1,8 +1,32 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from video_dl import download, download_audio
-import base64
+from pytube import YouTube
+
+VIDEO_SAVE_DIRECTORY = "./videos"
+AUDIO_SAVE_DIRECTORY = "./audio"
+
+
+def download(video_url):
+    video = YouTube(video_url)
+    video = video.streams.get_highest_resolution()
+
+    try:
+        video.download(VIDEO_SAVE_DIRECTORY)
+    except:
+        print("Failed to download video")
+
+    print("video was downloaded successfully")
+
+
+def download_audio(video_url):
+    video = YouTube(video_url)
+    audio = video.streams.filter(only_audio=True, file_extension='mp4').first()
+
+    try:
+        audio.download(AUDIO_SAVE_DIRECTORY)
+    except:
+        print("Failed to download audio")
+
+    print("audio was downloaded successfully")
 
 
 st.markdown(
