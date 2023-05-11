e4afd399c42f4ec116a1689f15ae815bcbfa275f
AdayaNezer <adaya1027@gmail.com>
2023-05-11 10:52:40 +0300

    Fix bug: can only insert a Youtube link

--- down_audio.py
+++ down_audio.py
@@ -6,18 +6,26 @@ from pytube import YouTube
 
 @st.cache_data(show_spinner=False, persist=True)
 def download_audio_to_buffer(url):
-    buffer = BytesIO()
-    youtube_video = YouTube(str(url))
-    audio = youtube_video.streams.get_audio_only()
-    default_filename = audio.default_filename
-    audio.stream_to_buffer(buffer)
-    return default_filename, buffer
+    try:
+        buffer = BytesIO()
+        youtube_video = YouTube(str(url))
+        audio = youtube_video.streams.get_audio_only()
+        default_filename = audio.default_filename
+        audio.stream_to_buffer(buffer)
+        return default_filename, buffer
+    except:
+        st.error(
+            "👉 Please only insert a YouTube link", icon="😡")
+        return -1, -1
 
 
 def download_audio(url):
     if url != '':
         with st.spinner("Downloading Audio Stream from Youtube..."):
             default_filename, buffer = download_audio_to_buffer(url)
+            if default_filename == -1 and buffer == -1:
+                st.error("❌💥💥💥💥💥💥❌")
+                return
         st.subheader("Title")
         st.write(default_filename)
         title_vid = Path(default_filename).with_suffix(".mp3").name
--- down_video.py
+++ down_video.py
@@ -7,18 +7,26 @@ from pathlib import Path
 
 @st.cache_data(show_spinner=False, persist=True)
 def download_video_to_buffer(url):
-    buffer = BytesIO()
-    youtube_video = YouTube(str(url))
-    video = youtube_video.streams.get_highest_resolution()
-    default_filename = video.default_filename
-    video.stream_to_buffer(buffer)
-    return default_filename, buffer
+    try:
+        buffer = BytesIO()
+        youtube_video = YouTube(str(url))
+        video = youtube_video.streams.get_highest_resolution()
+        default_filename = video.default_filename
+        video.stream_to_buffer(buffer)
+        return default_filename, buffer
+    except:
+        st.error(
+            "👉 Please only insert a YouTube link", icon="😡")
+        return -1, -1
 
 
 def download_video(url):
     if url != '':
         with st.spinner("Downloading video Stream from Youtube..."):
             default_filename, buffer = download_video_to_buffer(url)
+            if default_filename == -1 and buffer == -1:
+                st.error("❌💥💥💥💥💥💥❌")
+                return
         st.subheader("Title")
         st.write(default_filename)
         title_vid = Path(default_filename).with_suffix(".mp4").name
