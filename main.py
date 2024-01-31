08ecf08a6903817cd258ebcafb935ab92d0302ea
AdayaNezer <adaya1027@gmail.com>
2024-01-31 11:51:41 +0200

    change background

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ st.markdown(
     f"""
             <style>
             .stApp {{
-                background: url("https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/v546batch3-mynt-34-badgewatercolor_1.jpg?w=800&dpr=1&fit=default&crop=default&q=65&vib=3&con=3&usm=15&bg=F4F4F3&ixlib=js-2.2.1&s=89288ef4b47127f7f34a5998b50e4470.jpg");
+                background: url("https://images.rawpixel.com/image_social_landscape/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcm00MjItMDYzLWt6cGhnMjNrLmpwZw.jpg");
                 background-size: cover
             }}
             </style>
